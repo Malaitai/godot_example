cfecd3c9925d22fde361abef1dfc49ff8ac6d2cd
Malaitai <christian@asdah.school.nz>
2023-11-14 00:49:20 +1300

    Added animation

--- player.gd
+++ player.gd
@@ -7,8 +7,10 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim : AnimatedSprite2D = $AnimationSprite
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -22,7 +24,13 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		anim.play("run")
+		if direction > 0:
+			anim.flip_h = false
+		elif direction < 0:
+			anim.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play("idle")
 
 	move_and_slide()
